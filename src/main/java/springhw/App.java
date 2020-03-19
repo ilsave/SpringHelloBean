07f829cc4d9ff7c44171400732a4c8202fd08fb6
ilsave <bulldoo@yandex.ru>
2020-03-19 06:43:33 +0300

    first commit

---
+++ src/main/java/springhw/App.java
@@ -0,0 +1,20 @@
+package springhw;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import springhw.beans.HelloBean;
+import springhw.beans.Printer;
+
+public class App {
+
+	public static void main(String[] args) {
+		// Создаем контекст приложения из xml файла ("src/main/resources/applicationContext.xml")
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // Считываем бин (объект)
+        HelloBean hb = ctx.getBean("firstBean", HelloBean.class);
+        // Печатаем объект в консоль 
+        Printer.printMyFile(hb);
+
+        ctx.close();
+	}
+
+}
---
+++ src/main/java/springhw/beans/Printer.java
@@ -0,0 +1,9 @@
+package springhw.beans;
+
+public class Printer {
+    
+    public static void printMyFile(HelloBean helloBean){
+        System.out.println(helloBean.toString());
+    }
+
+}
